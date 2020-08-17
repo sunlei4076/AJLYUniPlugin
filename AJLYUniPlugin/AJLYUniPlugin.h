2a08093ea59d52aa2dbeeee7825038446cec117e
sun4076 <973797201@qq.com>
2020-08-17 11:01:42 +0800

    Initial Commit

---
+++ AJLYUniPlugin/AJLYUniPlugin.h
@@ -0,0 +1,19 @@
+//
+//  AJLYUniPlugin.h
+//  AJLYUniPlugin
+//
+//  Created by 孙磊 on 2020/8/17.
+//  Copyright © 2020 shebaochina. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AJLYUniPlugin.
+FOUNDATION_EXPORT double AJLYUniPluginVersionNumber;
+
+//! Project version string for AJLYUniPlugin.
+FOUNDATION_EXPORT const unsigned char AJLYUniPluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AJLYUniPlugin/PublicHeader.h>
+
+
